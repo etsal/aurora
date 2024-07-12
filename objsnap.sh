85f6fe34eb23762422bdaf97b483c5978fa4738c
Emil Tsalapatis <emil@etsalapatis.com>
2024-07-12 14:06:18 -0400

    objsnap.sh: Add usage error message

--- objsnap.sh
+++ objsnap.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+if [ ! -z $1 ]; then
+	DISK=$1
+fi
+
+if [ -z $DISK ]; then
+	echo "Disk not specified, please initialize the DISK variable or pass it as an argument"
+	exit 1
+fi
+
 kldload objsnap/objsnap.ko
 
 stat -x /dev/objsnap
