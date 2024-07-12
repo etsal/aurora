26155ebe8c0a5250b65d394ac152d73de22eadbb
Emil Tsalapatis <emil@etsalapatis.com>
2024-07-12 14:03:00 -0400

    new_objsnap: fix inconsistent indentation

--- tools/new_objsnap/new_objsnap.cc
+++ tools/new_objsnap/new_objsnap.cc
@@ -35,7 +35,7 @@ uint64_t clock_cycles;
 
 int newfs(const char *path)
 {
-    int status;
+	int status;
 	struct stat st;
 	int fd;
 	uint32_t bsize = 0;
@@ -105,36 +105,36 @@ int newfs(const char *path)
 
 
 	printf("creating super blocks\n");
-    super_t *sb = (super_t *)malloc(BLOCKSIZE);
+    	super_t *sb = (super_t *)malloc(BLOCKSIZE);
 	memset(sb, 0, ssize);
 	sb->super_ssize = ssize;
 	sb->super_bsize = 512;
 	sb->super_size = size / BLOCKSIZE;
 	sb->super_asize = bsize;
-    sb->super_max_inodes = MAXINODES;
-    sb->super_next = 1;
-    sb->super_version = 0;
-    sb->super_blk = 0;
+	sb->super_max_inodes = MAXINODES;
+	sb->super_next = 1;
+	sb->super_version = 0;
+	sb->super_blk = 0;
 
-    ssize_t written = write(fd, sb, BLOCKSIZE);
-    if (written == (-1)) {
-        perror("writing superblock failed");
-        free(sb);
-        return (1);
-    }
+	ssize_t written = write(fd, sb, BLOCKSIZE);
+	if (written == (-1)) {
+		perror("writing superblock failed");
+		free(sb);
+		return (1);
+	}
 
-    // Increment to cover the sister super block.
-    sb->super_blk = 1;
+	// Increment to cover the sister super block.
+	sb->super_blk = 1;
 
-    written = write(fd, sb, BLOCKSIZE);
-    if (written == (-1)) {
-        perror("writing second superblock failed");
-        free(sb);
-        return (1);
-    }
+	written = write(fd, sb, BLOCKSIZE);
+	if (written == (-1)) {
+		perror("writing second superblock failed");
+		free(sb);
+		return (1);
+	}
 
 	free(sb);
-    close(fd);
+	close(fd);
 
 	return (0);
 }
@@ -169,10 +169,10 @@ setup_map(struct mapping *map, size_t size_in_blocks)
 	map->inode = objsnap_create();
 	map->map = (char *)mmap(NULL, BLOCKSIZE * size_in_blocks, PROT_READ | PROT_WRITE, 
         MAP_ANON, -1, 0);
-    if (map->map == MAP_FAILED) {
-        printf("MMAP FAILED\n");
-        return -1;
-    }
+	if (map->map == MAP_FAILED) {
+		printf("MMAP FAILED\n");
+		return -1;
+	}
 
 	return (0);
 }
@@ -188,9 +188,9 @@ basicTest()
 	struct mapping map;
 	int error = 0;
 	if ((error = setup(disk))) {
-        printf("Problem in Setup!");
+		printf("Problem in Setup!");
 		return;
-    }
+	}
 
 	if ((error = setup_map(&map, 1024))) {
 		printf("Error in setting up mapping\n");
@@ -233,9 +233,9 @@ random_write_load(int num_objs, int size_of_obj_in_blocks,
 {
 	int error = 0;
 	if ((error = setup(disk))) {
-        printf("Problem in Setup!");
+		printf("Problem in Setup!");
 		return;
-    }
+	}
 
 	srand(time(NULL));
 
@@ -277,8 +277,8 @@ threadedTest(int numthreads, int num_objs,
 	int size_of_obj_in_blocks,
 	int writes_per_iteration, int times) {
 
-    // Vector to hold thread objects
-    std::vector<std::thread> threads;
+	// Vector to hold thread objects
+	std::vector<std::thread> threads;
 	uint64_t *avgs = (uint64_t *)malloc(sizeof(uint64_t)* numthreads);
 	int error = 0;
 
@@ -290,24 +290,24 @@ threadedTest(int numthreads, int num_objs,
 	}
 
 
-    for (int i = 0; i < numthreads; ++i) {
-        // Using a lambda function for the thread's task
-        threads.emplace_back([avgs, maps, num_objs, 
-			size_of_obj_in_blocks, writes_per_iteration, times, i](){
+	for (int i = 0; i < numthreads; ++i) {
+		// Using a lambda function for the thread's task
+		threads.emplace_back([avgs, maps, num_objs, 
+				size_of_obj_in_blocks, writes_per_iteration, times, i](){
 
-			uint64_t avg =  random_write_task(maps, num_objs, 
-				size_of_obj_in_blocks,
-				writes_per_iteration, 
-				times, i);
-			avgs[i] = avg;
+				uint64_t avg =  random_write_task(maps, num_objs, 
+					size_of_obj_in_blocks,
+					writes_per_iteration, 
+					times, i);
+				avgs[i] = avg;
 
-        });
-    }
+		});
+	}
 
-    // Join all threads to wait for them to finish
-    for (auto& thread : threads) {
-        thread.join();
-    }
+	// Join all threads to wait for them to finish
+	for (auto& thread : threads) {
+		thread.join();
+	}
 
 	uint64_t sum_avg = 0;
 	for (int i = 0; i < numthreads; i++) {
