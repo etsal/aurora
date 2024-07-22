b7781423cd2dd054274affff301ef57a6bd3ca86
Ryan Hancock <krhancoc@uwaterloo.ca>
2024-07-21 21:40:32 -0400

    Tool update

--- objsnap.sh
+++ objsnap.sh
@@ -9,10 +9,13 @@ if [ -z $DISK ]; then
 	exit 1
 fi
 
+
+kldunload objsnap > /dev/null 2> /dev/null
+
 kldload objsnap/objsnap.ko
 
 stat -x /dev/objsnap
 
-./tools/new_objsnap/new_objsnap $DISK $2
+./tools/new_objsnap/new_objsnap $DISK $2 $3
 
 kldunload objsnap
